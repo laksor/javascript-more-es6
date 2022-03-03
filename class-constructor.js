b46b7d3e4773542a8be1b26173ccb32e334a33d0
laskor <al.moksud@gmail.com>
2022-03-03 00:37:25 -0500

    class-constructor code done

---
+++ class-constructor.js
@@ -0,0 +1,17 @@
+
+// Class, constructor, method, create object from class
+
+class Support {
+    constructor(name, location) {
+        this.name = name;
+        this.location = location;
+    }
+    supportSesson(){
+        console.log(this.name, 'support the class');
+    }
+}
+const aamir = new Support('amir khan', 'BD');
+const salman = new Support('salman khan', 'USA');
+aamir.supportSesson();
+console.log(aamir);
+console.log(salman);
